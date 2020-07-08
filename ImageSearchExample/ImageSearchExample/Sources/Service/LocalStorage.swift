4285f57723d6f3bbd48eb66ffe3979176560ab31
pcugogo <pcugogo@gmail.com>
2020-07-08 22:06:26 +0900

    LocalStorage 간단한 수정

--- ImageSearchExample/ImageSearchExample/Sources/Service/LocalStorage.swift
+++ ImageSearchExample/ImageSearchExample/Sources/Service/LocalStorage.swift
@@ -9,20 +9,22 @@
 import Foundation
 
 struct LocalStorage {
-    struct Key {
-        static let favorites = "favorites"
+    enum Key: String {
+        case favorites
     }
+    
     var favorites: [String] {
-        guard let likeList = UserDefaults.standard.array(forKey: Key.favorites) as? [String] else { return [] }
-        return likeList
+        guard let list = UserDefaults.standard.array(forKey: Key.favorites.rawValue) as? [String] else { return [] }
+        return list
     }
+    
     func add(favorite: String) {
         var newList = favorites
         newList.append(favorite)
-        UserDefaults.standard.set(newList, forKey: Key.favorites)
+        UserDefaults.standard.set(newList, forKey: Key.favorites.rawValue)
     }
     func remove(favorite: String) {
         let newList = favorites.filter {$0 != favorite}
-        UserDefaults.standard.set(newList, forKey: Key.favorites)
+        UserDefaults.standard.set(newList, forKey: Key.favorites.rawValue)
     }
 }
