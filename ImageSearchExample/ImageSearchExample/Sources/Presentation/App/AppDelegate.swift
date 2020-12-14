2806d6f56dfcd6fea3eed15818245700d415c892
park <pcugogo@gmail.com>
2020-12-14 23:32:36 +0900

    간단한 수정

--- ImageSearchExample/ImageSearchExample/Sources/Presentation/App/AppDelegate.swift
+++ ImageSearchExample/ImageSearchExample/Sources/Presentation/App/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow()
+        window = UIWindow(frame: UIScreen.main.bounds)
         AppCoordinator(window: window!).start()
         return true
     }
